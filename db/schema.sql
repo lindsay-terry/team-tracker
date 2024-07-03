07fe77edbbfd348fafc915d66b7910796fbe72cb
Lindsay Terry <lindsaytee66@gmail.com>
2024-07-03 11:09:31 -0600

    Set up schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE tracker_db;
+
+\c team_tracker_db;
+
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    manager_id INTEGER NOT NULL,
+    FOREIGN KEY (manager_id)
+    references employees(id)
+)
